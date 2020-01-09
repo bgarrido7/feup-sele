864835033c9631e6405bc789ef509225bf1ec0dd
bgarrido7 <bianevesgarrido@gmail.com>
2020-01-09 21:09:32 +0000

    task2 include library dá mal

--- Final Project/sync_t4_b10/src/main.cpp
+++ Final Project/sync_t4_b10/src/main.cpp
@@ -1,12 +1,11 @@
+
+//#include <Adafruit_Sensor.h>   
+#include <TSL2561.h>
+
+light sensor tsl = TSL2561(TSL2561_ADDR_FLOAT, 12345);
+
 int lightSensorPin = A0;
 int analogValue = 0;
-
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_TSL2561_U.h>
-   
-Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
-
 
 // Motor A connections
 int enA = 9;
@@ -35,17 +34,9 @@ void displaySensorDetails(void)
 
 void configureSensor(void)
 {
-  /* You can also manually set the gain or enable auto-gain support */
-  // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
-  // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
-  tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
-  
-  /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
-  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+  tsl.enableAutoRange(true);  
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      
 
-  /* Update these values depending on what you've set above! */  
   Serial.println("------------------------------------");
   Serial.print  ("Gain:         "); Serial.println("Auto");
   Serial.print  ("Timing:       "); Serial.println("13 ms");
